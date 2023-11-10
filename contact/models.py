2d03497353e30694da59dd03d3059ae52c1248e0
VCruzSant <sviniantiago@gmail.com>
2023-11-09 23:03:31 -0300

    config new models for migration

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+# model gerencia as migrations
+
+
+class Contact(models.Model):
+    # Field -> campos
+    # CharField -> campo que limita caracteres
+    # blank = True -> parametro que torna o campo opicional
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50, blank=True)
+    phone = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50)
+    # pega a data no momento em que um contato é criado:
+    created_date = models.DateTimeField(default=timezone.now)
+    description = models.TextField(blank=True)
