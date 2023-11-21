7ff51dbda45ce10f11d499a6c3348b1a6a6962d8
VCruzSant <sviniantiago@gmail.com>
2023-11-21 09:11:11 -0300

    Config plural name with meta Class

--- contact/models.py
+++ contact/models.py
@@ -7,6 +7,10 @@ from django.utils import timezone
 
 
 class Category(models.Model):
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+
     name = models.CharField(max_length=50)
 
     # Com isso, consigo alterar o contact name que é exibido
