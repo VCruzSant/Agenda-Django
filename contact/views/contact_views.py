c4b9b8445ee77eca7f55c568f97b5dc811f0cd26
VCruzSant <sviniantiago@gmail.com>
2023-11-27 12:46:25 -0300

    Create partials for base and title variable

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -12,7 +12,8 @@ def index(request):
     print(contacts.query)
 
     context = {
-        'contacts': contacts
+        'contacts': contacts,
+        'site_title': 'Contatos - '
     }
     return render(
         request,
@@ -29,8 +30,11 @@ def contact(request, contact_id):
 
     single_contact = get_object_or_404(Contact, pk=contact_id, show=True)
 
+    site_title = f'{single_contact.first_name} {single_contact.last_name} - '
+
     context = {
-        'contact': single_contact
+        'contact': single_contact,
+        'site_title': site_title
     }
     return render(
         request,
