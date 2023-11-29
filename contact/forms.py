6bb54b63ff9e0823b5961fc322b68f8895493628
VCruzSant <sviniantiago@gmail.com>
2023-11-29 20:15:17 -0300

    Config inputs and widgets

--- contact/forms.py
+++ contact/forms.py
@@ -4,11 +4,40 @@ from contact.models import Contact
 
 
 class ContactForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # atualizando o widget com update -> função python para dicts:
+        self.fields['first_name'].widget.attrs.update(
+            {
+                'placeholder': 'Contact Name',
+            }
+        )
+        self.fields['last_name'].widget.attrs.update(
+            {
+                'placeholder': 'Contact Last Name',
+            }
+        )
+        self.fields['phone'].widget.attrs.update(
+            {
+                'placeholder': 'Contact Phone',
+            }
+        )
+
     class Meta:
         model = Contact
         fields = (
             'first_name', 'last_name', 'phone'
         )
+
+        # crio widgets:
+        # widgets = {
+        #     'first_name': forms.TextInput(
+        #         attrs={
+        #             'placeholder': 'Seu Nome'
+        #         }
+        #     )
+        # }
 
     def clean(self):
         # cleaned_data = self.cleaned_data
