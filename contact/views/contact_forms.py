74cf3ad0b079b8b4a4576abe0b24284a3376a3ec
VCruzSant <sviniantiago@gmail.com>
2023-11-29 11:37:49 -0300

    Validating errors in form

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,15 +1,11 @@
 from django import forms
 from django.core.paginator import Paginator
+from django.core.exceptions import ValidationError
 from django.db.models import Q
 from django.shortcuts import render, get_object_or_404, redirect
 
 from contact.models import Contact
 # Create your views here.
-
-# request.method GET -> requisição apenas para acessar a página
-# request.method POST -> requisição para enviar form
-# lógica: se for POST -> Envia formulário
-# lógica: se for GET -> Apenas renderiza a page
 
 
 class ContactForm(forms.ModelForm):
@@ -18,6 +14,24 @@ class ContactForm(forms.ModelForm):
         fields = (
             'first_name', 'last_name', 'phone'
         )
+
+    def clean(self):
+        cleaned_data = self.cleaned_data
+
+        self.add_error(
+            'first_name',
+            ValidationError(
+                'Menssage Error',
+                code='invalid'
+            )
+        )
+
+        return super().clean()
+
+# request.method GET -> requisição apenas para acessar a página
+# request.method POST -> requisição para enviar form
+# lógica: se for POST -> Envia formulário
+# lógica: se for GET -> Apenas renderiza a page
 
 
 def create(request):
