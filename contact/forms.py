53044a656aa19d41b4169f13f045a1c3cf771eff
VCruzSant <sviniantiago@gmail.com>
2023-12-04 15:20:29 -0300

    Config validantion for user form

--- contact/forms.py
+++ contact/forms.py
@@ -1,9 +1,8 @@
 from django import forms
-from django.core.exceptions import ValidationError
 from contact.models import Contact
+from django.core.exceptions import ValidationError
 from django.contrib.auth.forms import UserCreationForm
-
-from . import models
+from django.contrib.auth.models import User
 
 
 class ContactForm(forms.ModelForm):
@@ -77,4 +76,31 @@ class ContactForm(forms.ModelForm):
 
 
 class RegisterForm(UserCreationForm):
-    ...
+    first_name = forms.CharField(
+        required=True,
+        min_length=4
+    )
+    last_name = forms.CharField(
+        required=True,
+        min_length=4
+    )
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = (
+            'first_name', 'last_name', 'email',
+            'username', 'password1', 'password2'
+        )
+
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+
+        if User.objects.filter(email=email).exists():
+            self.add_error(
+                'email',
+                ValidationError(
+                    'Já existe usuários com esse e-mail', code='invalid'
+                )
+            )
+        return email
